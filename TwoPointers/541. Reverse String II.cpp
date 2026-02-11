315f4891eee6794663afa0bec49e670270348d39
Md.Solaiman <mdsolaimancse22026@gmail.com>
2026-02-11 10:33:14 +0600

    Added reverse string problem solution

---
+++ TwoPointers/541. Reverse String II.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+string reverseStr(string s, int k) {
+    int n = s.length();
+
+    for (int i = 0; i < n; i += 2 * k) {
+        int left = i;
+        int right = min(i + k - 1, n - 1);
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+
+    return s;
+}
+
+int main() {
+    string s = "abcdefg";
+    int k = 2;
+
+    cout << reverseStr(s, k);
+
+    return 0;
+}
